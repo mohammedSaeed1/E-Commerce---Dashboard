efb9c0fd838459a9f502e36c2ed2e919887482c8
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-16 14:34:05 +0200

    Create Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Register from './Components/Register/Register'
+import Dashboard from './Components/Dashboard/Dashboard'
 
+const router = createBrowserRouter(
+      [{ path: "/register", element: <Register />} ,
+      { path: "/dashboard", element: <Dashboard/> ,
+}])
 
 
 function App() {
 
-      <Register />
+    return  <RouterProvider router={router}></RouterProvider>
 
 }
 
