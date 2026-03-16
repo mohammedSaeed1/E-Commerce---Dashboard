eacc8c5ab8b140ceca8141e38427f29efcec8f0b
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-16 14:30:24 +0200

    Create dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
-import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
+
+
 
 function App() {
 
-  return (
-    <>
-    <Register/>
-    <Login/>
-    </>
-  )
+      <Register />
+
 }
 
 export default App
