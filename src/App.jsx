aca604a2e2a235f367ffddc2209cddfa87cd8ed6
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-17 09:28:40 +0200

    Create protected route component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Register from './Components/Register/Register'
-import Dashboard from './Components/Dashboard/Dashboard'
-import Categories from './Components/Categories/Categories'
-import SubCategories from './Components/SubCategories/SubCategories'
-import Products from './Components/Products/Products'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Register from "./Components/Register/Register";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Categories from "./Components/Categories/Categories";
+import SubCategories from "./Components/SubCategories/SubCategories";
+import Products from "./Components/Products/Products";
+import Login from "./Components/Login/Login";
 
-const router = createBrowserRouter([{ path: "", element: <Dashboard/>, children:[
-            {index: true, element: <Categories/>},
-            {path: "categories", element: <Categories/>},
-       {path: "subcategories", element: <SubCategories/>},
-      {path: "products", element: <Products/>}] },
-])
-
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Dashboard />,
+    children: [
+      { index: true, element: <Categories /> },
+      { path: "categories", element: <Categories /> },
+      { path: "subcategories", element: <SubCategories /> },
+      { path: "products", element: <Products /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Register /> },
+    ],
+  },
+]);
 
 function App() {
-
-    return  <RouterProvider router={router}></RouterProvider>
-
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
-export default App
+export default App;
